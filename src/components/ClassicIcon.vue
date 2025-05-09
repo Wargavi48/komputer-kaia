6e5a83efd22dd31c2bd6be6529a24ab5ee4c4229
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2025-05-09 11:14:21 +0700

    feat: added ClassicIcon component

---
+++ src/components/ClassicIcon.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <img
+    :src="`/icons/${props.name}.png`"
+    :alt="props.name"
+    class="aspect-square object-contain object-center"
+  />
+</template>
