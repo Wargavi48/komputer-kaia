9854874fd9f87f9619510e4f9891641dd80d9bfc
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2025-05-15 23:20:57 +0700

    feat: make ClassicButton a dynamic component

--- src/components/ClassicButton.vue
+++ src/components/ClassicButton.vue
@@ -1,9 +1,15 @@
+<script setup>
+const props = defineProps({
+  is: { default: 'button' },
+})
+</script>
 <template>
-  <button
+  <component
+    :is="props.is"
     class="border-2 min-w-4 px-1 bg-gray-200 active:bg-gray-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
   >
     <slot />
-  </button>
+  </component>
 </template>
 
 <style scoped>
